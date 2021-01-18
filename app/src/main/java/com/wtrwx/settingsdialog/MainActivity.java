4b5f6a7a457c46ca9ec0ba0d059695fbe586d8ec
Wtrwx <t156156@qq.com>
2021-01-18 16:38:16 +0800

    show dialog by a method

--- app/src/main/java/com/wtrwx/settingsdialog/MainActivity.java
+++ app/src/main/java/com/wtrwx/settingsdialog/MainActivity.java
@@ -10,11 +10,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        SettingsDialog dialog = new SettingsDialog();
-        dialog.show(getFragmentManager(), "SettingsDialog");
-
+        setContentView(R.layout.activity_main);      
+        showSettingsDialog("com.wtrwx.settingsdialog.SettingsFragment");
         // get value form sp
         SharedPreferences sp = getSharedPreferences("com.wtrwx.settingsdialog_preferences", MODE_PRIVATE);
         if (sp.getBoolean("setting_test1", false)) {
@@ -22,5 +19,11 @@ public class MainActivity extends Activity {
         }
 
     }
+
+    private void showSettingsDialog(String fragment) {
+        SettingsDialog dialog = new SettingsDialog();
+        dialog.show(getFragmentManager(), fragment);
+    }
+
 
 } 
